529f58c5934857b7757ab0cd731c0ace12fe2ad8
ni <nicolas.nkruger@gmail.com>
2026-04-27 19:44:01 -0300

    save

--- gen.bash
+++ gen.bash
@@ -1,3 +1,6 @@
 pandoc cv.md -o cv.pdf --pdf-engine=lualatex
 gs -o 'profile - Nícolas Krüger.pdf' -sDEVICE=pdfwrite -dFILTERALL profile.pdf
 pandoc profile.md -o profile.pdf --pdf-engine=pdflatex
+
+pandoc cv_pt-br.md -o cv_pt-br.pdf --pdf-engine=lualatex
+gs -o 'cv_Nícolas Krüger.pdf' -sDEVICE=pdfwrite -dFILTERALL cv_pt-br.pdf
